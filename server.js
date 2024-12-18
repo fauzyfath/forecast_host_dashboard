9d8ba380cb1a02c3d16978cb2a0deb8c7260fffb
fauzyfath <fauzy.fyz1@gmail.com>
2024-12-18 19:10:16 +0800

    new data dashboard

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ connection.connect((err) => {
 });
 
 // API Endpoint untuk data
+// API Endpoint untuk data
 app.get("/api/weather", (req, res) => {
   const query = `
     SELECT 
@@ -35,13 +36,19 @@ app.get("/api/weather", (req, res) => {
       r.curah_hujan_per_jam,
       r.curah_hujan_hari_ini,
       w.kecepatan_kilometer_per_jam,
-      l.lux_value
+       w.kecepatan_meter_per_detik,
+      w.rotasi_per_detik,
+      l.lux_value,
+      l.cahaya_cuaca,
+      d.distance
     FROM 
       rainfall r
     JOIN 
       wind w ON r.timestamp = w.timestamp
     JOIN 
       light l ON r.timestamp = l.timestamp
+    JOIN 
+      distance d ON r.timestamp = d.timestamp
     ORDER BY r.timestamp DESC
     LIMIT 10;
   `;
